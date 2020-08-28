a818635bbe9ed8f59e9d1a5cf76716fabac18cda
Josh <36907139+OptimusPrinceps@users.noreply.github.com>
2020-08-28 18:20:10 +1200

    Added dataset class and tools for batching

--- data_prep.py
+++ data_prep.py
@@ -1,32 +1,49 @@
 import json 
 from generate_vocab import UNK, PAD
+import torch
 
-def loadData(f_pth="./data/toy-data.txt"):
-	with open(f_pth, 'r') as f:
-		# Each element of X is [ast, index]
-		# where index is the starting position of the unseen nodes
-		# See separate_dps in utils.py for more details
-		X = [json.loads(line) for line in f]
-	y = [x[0].pop() for x in X]
-	return X, y
-
-class Tokeniser():
-	def __init__(self, vocab_filepath, max_len=100000):
+class Tokeniser(object):
+	def __init__(self, vocab_filepath, max_len=1000):
 		vocab = json.load(open(vocab_filepath))
 		self.vocab = {vocab[i]: i for i in range(len(vocab))}
 		self.vocab.update({i: vocab[i] for i in range(len(vocab))})
 		self.max_len = max_len
-		self.UNK, self.PAD = UNK, PAD
+		self.unk, self.pad = self.vocab[UNK], self.vocab[PAD]
 	
-	def encode(self, sequence):
-		return [self.vocab.get(s, self.UNK) for s in sequence] + \
-				[self.PAD for _ in range(self.max_len-len(sequence))]
+	def encode_value(self, val):
+		return self.vocab.get(val, self.unk)
+
+	def encode_seq(self, sequence):
+		return [self.vocab.get(s, self.unk) for s in sequence] + \
+				[self.pad for _ in range(self.max_len-len(sequence))]
 	
 	def decode(self, q):
 		return self.vocab[q]
 	
+	def encode_batch(self, X, Y, tensor=True):
+		encoded_X = [self.encode_seq(x) for x in X]
+		encoded_Y = [self.encode_value(y) for y in Y]
+		if tensor:
+			return torch.tensor(encoded_X, dtype=torch.long), torch.tensor(encoded_Y, dtype=torch.long) 
+		return encoded_X, encoded_Y
+	
+class Dataset(object):
+	def __init__(self, f_pth="./data/toy-data.txt"):
+		self.loadData(f_pth)
+		self.batchIndex = 0
 
-
-def prepBatchData(X, y):
-	# Function for tokenising elements of X and y
-	pass
+	def loadData(self, f_pth):
+		with open(f_pth, 'r') as f:
+			# Each element of X is [ast, index]
+			# where index is the starting position of the unseen nodes
+			# See separate_dps in utils.py for more details
+			self.X = [json.loads(line) for line in f]
+			self.y = [x[0][-1] for x in self.X]
+			
+	def getBatch(self, batch_size):
+		# Function for tokenising elements of X and y
+		BI = self.batchIndex
+		x_batch = [x[0][:-1] for x in self.X[BI:BI+batch_size]]
+		y_batch = self.y[BI:BI+batch_size]
+		self.batchIndex += batch_size
+		return x_batch, y_batch
--- train.py
+++ train.py
@@ -2,22 +2,29 @@ from model import TransformerModel
 from torch.nn import CrossEntropyLoss
 import torch
 import numpy as np
-from data_prep import Tokeniser
+from data_prep import Tokeniser, Dataset
 np.random.seed(0)
 
-layer_norm_epsilon = 1e-6
-n_head = 2 #6
 n_ctx = 1000
-n_embd = 300
-n_layer = 2 #6
-loss_fn = CrossEntropyLoss
-vocab_size = 50000 #100000
-criterion = CrossEntropyLoss
-t = TransformerModel(vocab_size, loss_fn, n_layer, n_embd, n_ctx, n_head, layer_norm_epsilon)
+def buildModel():
+	layer_norm_epsilon = 1e-6
+	n_head = 2 #6
+	n_embd = 300
+	n_layer = 2 #6
+	loss_fn = CrossEntropyLoss
+	vocab_size = 50002 #100000
+	return TransformerModel(vocab_size, loss_fn, n_layer, n_embd, n_ctx, n_head, layer_norm_epsilon)
+
+t = buildModel()
 t.train()
-tokeniser = Tokeniser('vocab50k.json', max_len=vocab_size)
-X = np.round(np.random.rand(10,1000)*1000)
-y = np.round(np.random.rand(10)*1000)
+t.zero_grad()
+
+tokeniser = Tokeniser('vocab50k.json', max_len=n_ctx)
+data = Dataset()
+
+X, y = data.getBatch(batch_size=5)
+X, y = tokeniser.encode_batch(X, y, tensor=True)
+
 X = torch.tensor(X, dtype=torch.long)
 y = torch.tensor(y, dtype=torch.long)
 
