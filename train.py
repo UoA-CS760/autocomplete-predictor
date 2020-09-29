f33b772d46e5c28be3c6ab3e2f05f41cdc2cbf45
Josh <36907139+OptimusPrinceps@users.noreply.github.com>
2020-09-29 14:57:38 +1300

    y_hot encoding

--- train.py
+++ train.py
@@ -6,27 +6,29 @@ from data_prep import Tokeniser, Dataset
 np.random.seed(0)
 
 n_ctx = 1000
+vocab_size = 50002 #100000
 def buildModel():
 	layer_norm_epsilon = 1e-6
 	n_head = 2 #6
 	n_embd = 300
 	n_layer = 2 #6
 	loss_fn = CrossEntropyLoss
-	vocab_size = 50002 #100000
 	return TransformerModel(vocab_size, loss_fn, n_layer, n_embd, n_ctx, n_head, layer_norm_epsilon)
 
 t = buildModel()
 t.train()
 t.zero_grad()
 
-tokeniser = Tokeniser('vocab50k.json', max_len=n_ctx)
+tokeniser = Tokeniser('vocab50k.json', max_seq_len=n_ctx)
 data = Dataset()
 
-X, y = data.getBatch(batch_size=5)
+batch_size=20
+# for _ in range(training_steps):
+X, y = data.getBatch(batch_size=batch_size)
 X, y = tokeniser.encode_batch(X, y, tensor=True)
-
-X = torch.tensor(X, dtype=torch.long)
-y = torch.tensor(y, dtype=torch.long)
+y_hot = torch.zeros(batch_size, vocab_size, dtype=torch.bool)
+for i, j in enumerate(y):
+	y_hot[i,j] = True
 
 output = t(X, y)
-print(output.size())
+print(output.shape)
